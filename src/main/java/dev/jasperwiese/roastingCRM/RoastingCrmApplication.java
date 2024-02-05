1c1f34b620c3e14d60223639ea1a68aadb70bc67
jasperwiese <jasper.wiese@protonmail.com>
2024-02-05 10:04:36 +0200

    springboot init

---
+++ src/main/java/dev/jasperwiese/roastingCRM/RoastingCrmApplication.java
@@ -0,0 +1,13 @@
+package dev.jasperwiese.roastingCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoastingCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoastingCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jasperwiese/roastingCRM/RoastingCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jasperwiese.roastingCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoastingCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
