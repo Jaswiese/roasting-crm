2bca624772583537eadc3fb1953099b427e77ac3
Jasper Wiese <jasperwiese@jasperwiese.dev>
2024-03-19 14:28:33 +0200

    TargetTemperatureDto validation checks

--- src/main/java/dev/jasperwiese/roastingCRM/dto/TargetTemperatureDto.java
+++ src/main/java/dev/jasperwiese/roastingCRM/dto/TargetTemperatureDto.java
@@ -1,5 +1,7 @@
 package dev.jasperwiese.roastingCRM.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +12,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TargetTemperatureDto {
+
     private String targetTemperatureId;
+
+    @NotEmpty(message = "preheat cannot be empty")
+    @NotNull(message = "preheat cannot be null")
     private String preheat;
+
+    @Notn
     private String firstCrack;
     private String development;
     private String drop;
