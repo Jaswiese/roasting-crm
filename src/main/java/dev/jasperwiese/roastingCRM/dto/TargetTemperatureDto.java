30067a58fa91b738b8d55e5d65954fb84314bfb3
Jasper Wiese <jasperwiese@jasperwiese.dev>
2024-03-19 16:38:11 +0200

    added basic validation for AirflowSettingsDto, RoastingProfileDto TargetTemperatureDto.

--- src/main/java/dev/jasperwiese/roastingCRM/dto/TargetTemperatureDto.java
+++ src/main/java/dev/jasperwiese/roastingCRM/dto/TargetTemperatureDto.java
@@ -2,6 +2,7 @@ package dev.jasperwiese.roastingCRM.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,16 +12,41 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+/*
+* */
 public class TargetTemperatureDto {
 
     private String targetTemperatureId;
 
-    @NotEmpty(message = "preheat cannot be empty")
-    @NotNull(message = "preheat cannot be null")
+    @NotEmpty(message = "preheat cannot be empty.")
+    @NotNull(message = "preheat cannot be null.")
+    @Pattern(
+            regexp = "\\d{3}",
+            message = "pre heat can only contain digits and is limited to a length of 3 digits."
+    )
     private String preheat;
 
-    @Notn
+    @NotEmpty(message = "first crack cannot be empty.")
+    @NotNull(message = "first crack cannot be null.")
+    @Pattern(
+            regexp = "\\d{3}",
+            message = "first crack can only contain digits and is limited to a length of 3 digits."
+    )
     private String firstCrack;
+
+    @NotEmpty(message = "development cannot be empty.")
+    @NotNull(message = "development cannot be null.")
+    @Pattern(
+            regexp = "\\d{3}",
+            message = "development can only contain digits and is limited to a length of 3 digits."
+    )
     private String development;
+
+    @NotEmpty(message = "drop cannot be empty.")
+    @NotNull(message = "drop cannot be null.")
+    @Pattern(
+            regexp = "\\d{3}",
+            message = "drop can only contain digits and is limited to a length of 3 digits."
+    )
     private String drop;
 }
